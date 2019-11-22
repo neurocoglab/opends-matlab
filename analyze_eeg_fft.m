e872bbaed753728364bc973f21a8a0591b8faacd
DESKTOP-9SUBJ6C\Marije <marijeterwal@gmail.com>
2019-11-22 19:48:11 +0000

    FFT for left and right lane changes

---
+++ analyze_eeg_fft.m
@@ -0,0 +1,104 @@
+function results_fft = analyze_eeg_fft(cfg,data)
+
+%% check config
+
+% frequencies
+if ~isfield(cfg,'freq')
+    error('No frequencies specified')
+end
+
+% method
+if ~isfield(cfg, 'method')
+    cfg.method = 'fft';
+end
+if strcmp(cfg.method, 'fft') && length(cfg.freq)>2 %
+    warning('Reducing freqs to foi')
+    cfg.foi = [cfg.freq(1),cfg.freq(end)];
+end
+
+% 1/f correction?
+if ~isfield(cfg, 'fcor')
+    cfg.fcor = false;
+end
+
+% time selection?
+if ~isfield(cfg,'toi')
+    warning('No toi specified, computing fft for whole trial')
+    cfg.toi = [];
+end
+
+% channel selection?
+if ~isfield(cfg,'channels')
+    cfg.channels = 1:length(data.label);
+end
+
+% trial selection?
+if ~isfield(cfg,'trials')
+    cfg.trials = 1:length(data.trial);
+end
+
+%% compute spectra
+
+% prep output struct
+results_fft = rmfield(data, 'trial');
+
+% prep for loop
+ntrials = length(cfg.trials);
+nchannels = length(cfg.channels);
+
+for tr = 1:ntrials
+            
+        if ~isempty(cfg.toi)
+            tlog = data.time{cfg.trials(tr)}>=cfg.toi(1) & data.time{cfg.trials(tr)}<=cfg.toi(2);
+        else
+            tlog = ones(size(data.time));
+        end
+
+    for ch = 1:nchannels
+        % ignore NaNs
+        tid = tlog & ~isnan(data.trial{cfg.trials(tr)}(cfg.channels(ch),:));
+        
+        % compute spectrum
+        switch cfg.method
+            case 'fft'
+                dumfft = fft(data.trial{cfg.trials(tr)}(cfg.channels(ch),tid));
+                freqs = linspace(0,data.fsample,length(dumfft));
+                fid = find(freqs>=cfg.foi(1),1,'first'):find(freqs>=cfg.foi(2),1,'first');
+                datspctrm = abs(dumfft(fid));
+                freqs = freqs(fid);
+            case 'pmtm' 
+%                 [dumfft,freqs] = pmtm(data.trial{cfg.trials(tr)}(cfg.channels(ch),tid),2,...
+%                     sum(tid),data.fsample);
+%                 fid = find(freqs>=cfg.foi(1),1,'first'):find(freqs>=cfg.foi(2),1,'first');
+%                 datspctrm = dumfft(fid);
+%                 freqs = freqs(fid);
+                freqs = cfg.freq;
+                datspctrm = pmtm(data.trial{cfg.trials(tr)}(cfg.channels(ch),tid),5,...
+                    freqs,data.fsample);
+        end
+        
+%         figure; 
+%         plot(freqs,datspctrm)
+        
+        % do 1/f correction
+        if cfg.fcor
+            % fit 1/f - is this robust?
+            p = polyfit(log10(freqs), reshape(log10(datspctrm),size(freqs)), 1);
+            
+            % remove 1/f
+            datspctrm = datspctrm - reshape(10.^(log10(freqs)*p(1)+p(2)),size(datspctrm));
+            
+%             hold on; plot(freqs,reshape(10.^(log10(freqs)*p(1)+p(2)),size(datspctrm)),'linewidth',2)
+        end
+        
+        % store data
+        results_fft.powspctrm{tr}(ch,:) = datspctrm;
+    end
+end
+
+%% admin
+results_fft.freq = freqs;
+cfg.freq = freqs;
+results_fft.cfg_fft = cfg;
+
+end
