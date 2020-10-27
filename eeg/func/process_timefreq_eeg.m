1cfd6d126a58dba7a1a6465128338ba164e94c39
Andrew Reid <typically@yahoo.com>
2020-10-27 18:41:06 +0000

    Update to select_data_eeg.m

--- eeg/func/process_timefreq_eeg.m
+++ eeg/func/process_timefreq_eeg.m
@@ -23,39 +23,39 @@ end
 
 results = get_timefreq_trials_eeg( params, data, results );
 
-outdir = sprintf( '%s/%s', params.io.output_dir, subject );
-fft_file = sprintf('%s/results_fft_eeg.m', outdir);
-
-if exist(fft_file, 'file') && ~params.eeg.timefreq.regen_fft
-    % Load existing FFT results
-    load( fft_file );
-    
-else
-    
-    % Compute time/frequency for each round
-    cfg = [];
-    cfg.toi = [];
-    cfg.Zscore = false;
-    cfg.dt = params.eeg.timefreq.dt;
-    cfg.foi = params.eeg.timefreq.foi;
-    cfg.wavelet = params.eeg.timefreq.wavelet;
-
-    results.eeg.timefreq = process_wavelet_eeg(cfg, results.eeg.timefreq, [], params.general.debug);
-
-    % Save FFT results
-    if params.general.debug
-       fprintf(' * Saving FFT to %s...', fft_file);
-    end
-    save( fft_file, 'results' );
-    if params.general.debug
-       fprintf('done.\n');
-    end
-end
-
+% outdir = sprintf( '%s/%s', params.io.output_dir, subject );
+% fft_file = sprintf('%s/results_fft_eeg.m', outdir);
+% 
+% if exist(fft_file, 'file') && ~params.eeg.timefreq.regen_fft
+%     Load existing FFT results
+%     load( fft_file );
+%     
+% else
+%     
+%     Compute time/frequency for each round
+%     cfg = [];
+%     cfg.toi = [];
+%     cfg.Zscore = false;
+%     cfg.dt = params.eeg.timefreq.dt;
+%     cfg.foi = params.eeg.timefreq.foi;
+%     cfg.wavelet = params.eeg.timefreq.wavelet;
+% 
+%     results.eeg.timefreq = process_wavelet_eeg(cfg, results.eeg.timefreq, [], params.general.debug);
+% 
+%     Save FFT results
+%     if params.general.debug
+%        fprintf(' * Saving FFT to %s...', fft_file);
+%     end
+%     save( fft_file, 'results' );
+%     if params.general.debug
+%        fprintf('done.\n');
+%     end
+% end
+% 
 % Perform statistical analyses
-results = analyze_timefreq_eeg( params, data, results );
-
-
+% results = analyze_timefreq_eeg( params, data, results );
+% 
+% 
 
 end
 
